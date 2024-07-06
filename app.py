64b07be27b09774ae0837a547f660459ed07b240
AcilRestu12 <arieskarestu214@gmail.com>
2024-07-07 04:20:10 +0700

    front-end -> create sidebar, upload image & result page

--- app.py
+++ app.py
@@ -7,5 +7,13 @@ app = Flask(__name__)
 def index():
 	return render_template("index.html")
 
+@app.route("/upload")
+def upload():
+	return render_template("upload/index.html")
+
+@app.route("/result")
+def result():
+	return render_template("upload/result.html")
+
 if __name__ == '__main__':
 	app.run(debug=True)
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,23 @@ module.exports = {
 			"./node_modules/flowbite/**/*.js"
 	],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: '#003C43',
+				secondary: '#E3FEF7',
+				third: '#77B0AA',
+				fourth: '#E3FEF7',
+				text: '#F5F5F5',
+				// text2: '#190482',
+				// text: '#190482',
+			},
+			fontFamily: {
+				raleway: ["Raleway", 'sans-serif'],
+				pacifio: ["Pacifico", 'cursive'],
+				vibur: ["Vibur", 'cursive'],
+				montserrat: ['Montserrat', 'sans-serif'],
+			},
+		},
 	},
 	plugins: [
 		require("flowbite/plugin")
